0a6ecc1ef1dff73c71dd7bbfbe60eb9ee0b60526
Krishna Bhadauria <103627548+Krishnabhadauria@users.noreply.github.com>
2024-08-12 00:44:17 +0530

    updated

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ import numpy as np
 
 model = pickle.load(open('./model.sav','rb'))
 
-st.title('Energy Consumption Prediction')
-st.sidebar.header('Location Data')
+st.set_page_config(page_title="Energy Consumption Prediction", layout="centered", initial_sidebar_state="expanded")
+
+
+
+st.title('🔋Energy Consumption Prediction')
+st.sidebar.header('📍 Input Location Data')
 
 def user_input():
-    Temperature = st.sidebar.number_input("Temperature")
-    Humidity = st.sidebar.number_input("Humidity")
-    SquareFootage = st.sidebar.number_input("Square Footage")
-    Occupancy = st.sidebar.slider('Occupancy', 1,50, 1)
-    HVACUsage = st.sidebar.selectbox("HVAC Usage", [0, 1])
-    LightingUsage = st.sidebar.selectbox("Lighting Usage", [0, 1])
-    RenewableEnergy = st.sidebar.number_input("Renewable Energy")
-    DayOfWeek = st.sidebar.slider('DayOfWeek', 0, 6, 1)
-    Holiday	= st.sidebar.selectbox("Holiday", [0, 1])
-    Year = st.sidebar.number_input("Year")
-    Month = st.sidebar.number_input("Month")
-    Day = st.sidebar.number_input("Day")
-    Hour = st.sidebar.number_input("Hour")
-    Minute = st.sidebar.number_input("Minute")
-    Second = st.sidebar.number_input("Second")
+    Temperature = st.sidebar.number_input("🌡️ Temperature ")
+    Humidity = st.sidebar.number_input("💧 Humidity")
+    SquareFootage = st.sidebar.number_input("🏢 Square Foot Area")
+    Occupancy = st.sidebar.slider('👥 Occupancy', 1,50, 1)
+    HVACUsage = st.sidebar.selectbox("❄️ HVAC Usage", [0, 1])
+    LightingUsage = st.sidebar.selectbox("💡Lighting Usage", [0, 1])
+    RenewableEnergy = st.sidebar.number_input("🌍 Renewable Energy")
+    DayOfWeek = st.sidebar.slider('📅 DayOfWeek', 0, 6, 1)
+    Holiday	= st.sidebar.selectbox("🎉 Holiday", [0, 1])
+    Year = st.sidebar.number_input("📅 Year")
+    Month = st.sidebar.number_input("📅 Month")
+    Day = st.sidebar.number_input("📅 Day")
+    Hour = st.sidebar.number_input("⏰ Hour")
+    Minute = st.sidebar.number_input("⏰ Minute")
+    Second = st.sidebar.number_input("⏰ Second")
 
     user_input_data = {
         'Temperature': Temperature,
@@ -51,5 +55,5 @@ st.header('User Data')
 st.write(user_data)
 
 consumption=model.predict(user_data)
-st.subheader('Energy Consumption:')
+st.subheader('⚡Energy Consumption Predicted:')
 st.subheader(consumption[0])
